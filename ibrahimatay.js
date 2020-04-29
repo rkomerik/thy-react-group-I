99f3295aebda9896515f1c1d52ad302b6fb13b1b
İbrahim ATAY <contact@ibrahimatay.com>
2020-04-29 10:41:01 +0300

    Update ibrahimatay.js

--- ibrahimatay.js
+++ ibrahimatay.js
@@ -4,45 +4,21 @@ const products = [
         type:3,
         price:65.0,
         count:7,
-<<<<<<< HEAD
-<<<<<<< HEAD
-        totalAmout: function(){ this.count * this.price;} 
-=======
         totalAmout: function(){ return this.count * this.price;} 
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
-=======
-        totalAmout: function(){ return this.count * this.price;} 
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
     },
     {
         id:2,
         type:0,
         price:42.3,
         count:4,
-<<<<<<< HEAD
-<<<<<<< HEAD
-        totalAmout: function(){ this.count * this.price;} 
-=======
         totalAmout: function(){ return this.count * this.price;} 
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
-=======
-        totalAmout: function(){ return this.count * this.price;} 
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
     },
     {
         id:3,
         type:1,
         price:65.0,
         count:5,
-<<<<<<< HEAD
-<<<<<<< HEAD
-        totalAmout: function(){ this.count * this.price;} 
-=======
         totalAmout: function(){ return this.count * this.price;} 
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
-=======
-        totalAmout: function(){ return this.count * this.price;} 
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
     },
 ];
 
@@ -59,12 +35,5 @@ function calculator(products){
 
 let result = calculator(products);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 console.log(result);
-=======
-console.log(result);
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
-=======
-console.log(result);
->>>>>>> 98f025a2865ac62c1b62b77b7fbf58fbabf2eb89
+
