972361c46e03fc27816f2bdd63a6f73d2f5ffcdd
SUMEYYE ERTAS SULU (Bilgi Teknolojileri Baskanligi (Yazilim Gelistirme Md.) - Muhendis) <S_ERTAS5@thynet.thy.com>
2020-04-29 10:37:42 +0300

    sumeyye

---
+++ sumeyye.js
@@ -0,0 +1,24 @@
+var products=[];
+var product1 = {id:1, type:"test", price:46,
+profit: function ()
+{
+    return this.price * 1.2
+}
+};
+products[0]=product1;
+var product2 = {id:2, type:"test2", price:66,
+profit: function ()
+{
+    return this.price * 1.2
+}
+};
+products[2]=product2;
+
+function myFunc(params)
+{
+    let total=0;
+    params.forEach(p =>  total=total + p.profit());
+    console.log(total);
+};
+
+myFunc(products);
