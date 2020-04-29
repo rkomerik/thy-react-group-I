b2afdf48c1bf57357a40c6792ecb2fb2d95182d7
salihsahin <salihsahin@thy.com>
2020-04-29 11:02:05 +0300

    Code Fix

--- salihsahin.js
+++ salihsahin.js
@@ -1,25 +1,17 @@
+let profit = (price) => price * 1.1;
+let callFunction = (products) =>
+  products.forEach((product) => console.log(profit(product.price)));
+
 const products = [
   {
     id: 1,
     type: 0,
     price: 65.0,
-    profit: function () {
-      return this.price * 1.1;
-    },
   },
   {
     id: 2,
     type: 0,
     price: 24.99,
-    profit: function () {
-      return this.price * 1.1;
-    },
   },
 ];
-
-let callFunction = (products) => {
-  products.forEach((product) => {
-    console.log(product.profit());
-  });
-};
 callFunction(products);
