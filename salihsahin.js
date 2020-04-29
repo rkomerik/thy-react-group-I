89bdb9d58eaa9c54e8df2b7652b746963392e4c0
salihsahin <salihsahin@thy.com>
2020-04-30 00:52:42 +0300

    Minify

--- salihsahin.js
+++ salihsahin.js
@@ -1,14 +1,6 @@
 const products = [
-  {
-    id: 1,
-    type: 0,
-    price: 65.0,
-  },
-  {
-    id: 2,
-    type: 0,
-    price: 24.99,
-  },
+  { id: 1, type: 0, price: 65.0 },
+  { id: 2, type: 0, price: 24.99 },
 ];
 let profit = (price) => price * 1.1;
 let callFunction = (products) =>
