09132e472051c8e36e2a7c96f770dec203b83191
mganitombalak <mgani.tombalak@yahoo.com>
2020-05-01 17:14:36 +0300

    Axios Impl

--- React/test/src/pages/cat-mgmt/components/list.js
+++ React/test/src/pages/cat-mgmt/components/list.js
@@ -1,69 +1,26 @@
 import React from 'react';
 import DataTable from '../../../components/datacompenents/datatable'
-
+import axios from 'axios'
 export default class List extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+    }
+    componentDidMount() {
+        axios.get("http://178.128.248.160:81/api/unit",{
+            headers:{
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjJjYTc5NjVhLTk3NWQtNGNiOC05OGNjLTIzZDg5Y2M5YzU3YiIsImVtYWlsIjoibWdhbmkudG9tYmFsYWtAeWFob28uY29tIiwicm9sZSI6Im51bGwiLCJuYmYiOjE1ODgzMzk4NzEsImV4cCI6MTU4ODM0MDc3MSwiaWF0IjoxNTg4MzM5ODcxfQ.p3qEfTGQgzwtIyhweWNlFH_PAGac1gvMhTwsnpY1tIc`
+            }
+        })
+            .then(res => this.setState({data:res.data.data}))
+            .catch(e => console.log(e));
+    }
     render() {
-        let data = [
-            {
-                "name": "Santimetre",
-                "shortForm": "[cm]",
-                "materials": null,
-                "id": "9094b257-5451-4cf8-940d-9e1780093013",
-                "isActive": true,
-                "isDeleted": false,
-                "createdAt": "2019-10-23T09:53:39.923614Z Etc/UTC",
-                "updatedAt": null,
-                "deletedAt": null
-            },
-            {
-                "name": "Top",
-                "shortForm": "[top]",
-                "materials": null,
-                "id": "cd555632-c7dd-4299-bb5b-1381e2d13fad",
-                "isActive": true,
-                "isDeleted": false,
-                "createdAt": "2019-10-23T09:53:49.616478Z Etc/UTC",
-                "updatedAt": "2019-10-23T09:53:56.22024Z Etc/UTC",
-                "deletedAt": null
-            },
-            {
-                "name": "Deste",
-                "shortForm": "[d]",
-                "materials": null,
-                "id": "143e86a5-3284-44ba-86a7-da55f558f463",
-                "isActive": true,
-                "isDeleted": false,
-                "createdAt": "2019-10-23T09:54:08.23499Z Etc/UTC",
-                "updatedAt": null,
-                "deletedAt": null
-            },
-            {
-                "name": "Kilogram",
-                "shortForm": "[kg]",
-                "materials": null,
-                "id": "c76baa0a-2078-4a0e-9ae5-9c1f28fdb922",
-                "isActive": true,
-                "isDeleted": false,
-                "createdAt": "2019-10-23T09:54:18.899245Z Etc/UTC",
-                "updatedAt": "2019-10-23T09:54:27.063285Z Etc/UTC",
-                "deletedAt": null
-            },
-            {
-                "name": "Metre",
-                "shortForm": "[m]",
-                "materials": null,
-                "id": "7488252e-6957-4663-a189-9a5b2d9b03b7",
-                "isActive": true,
-                "isDeleted": false,
-                "createdAt": "2019-10-23T09:54:38.083757Z Etc/UTC",
-                "updatedAt": null,
-                "deletedAt": null
-            }];
         return (
             <div className="ui container">
-                <DataTable headerText={['Name', 'ShortForm', 'Materials','id','Active','Deleted','Created At','Updated At','Deleted At']}
-                 data={data} />
+                <DataTable headerText={['Name', 'ShortForm', 'Materials', 'id', 'Active', 'Deleted', 'Created At', 'Updated At', 'Deleted At']}
+                    data={this.state.data} />
             </div>
         )
     }
