56b07bab4f9ff66a4af53cbcfc21622cd91b17cb
mganitombalak <mgani.tombalak@yahoo.com>
2020-05-01 16:03:26 +0300

    Keys Term/ Custom DataTable Impl

--- React/test/src/pages/cat-mgmt/components/list.js
+++ React/test/src/pages/cat-mgmt/components/list.js
@@ -1,39 +1,70 @@
 import React from 'react';
+import DataTable from '../../../components/datacompenents/datatable'
 
+export default class List extends React.Component {
 
-const List = () => {
-    return (
-        <table class="ui celled table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Status</th>
-                    <th>Notes</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="disabled">
-                    <td>Jamie</td>
-                    <td>Approved</td>
-                    <td>Requires call</td>
-                </tr>
-                <tr>
-                    <td>John</td>
-                    <td>Selected</td>
-                    <td>None</td>
-                </tr>
-                <tr>
-                    <td>Jamie</td>
-                    <td>Approved</td>
-                    <td>Requires call</td>
-                </tr>
-                <tr>
-                    <td class="disabled">Jill</td>
-                    <td>Approved</td>
-                    <td>None</td>
-                </tr>
-            </tbody>
-        </table>
-    )
+    render() {
+        let data = [
+            {
+                "name": "Santimetre",
+                "shortForm": "[cm]",
+                "materials": null,
+                "id": "9094b257-5451-4cf8-940d-9e1780093013",
+                "isActive": true,
+                "isDeleted": false,
+                "createdAt": "2019-10-23T09:53:39.923614Z Etc/UTC",
+                "updatedAt": null,
+                "deletedAt": null
+            },
+            {
+                "name": "Top",
+                "shortForm": "[top]",
+                "materials": null,
+                "id": "cd555632-c7dd-4299-bb5b-1381e2d13fad",
+                "isActive": true,
+                "isDeleted": false,
+                "createdAt": "2019-10-23T09:53:49.616478Z Etc/UTC",
+                "updatedAt": "2019-10-23T09:53:56.22024Z Etc/UTC",
+                "deletedAt": null
+            },
+            {
+                "name": "Deste",
+                "shortForm": "[d]",
+                "materials": null,
+                "id": "143e86a5-3284-44ba-86a7-da55f558f463",
+                "isActive": true,
+                "isDeleted": false,
+                "createdAt": "2019-10-23T09:54:08.23499Z Etc/UTC",
+                "updatedAt": null,
+                "deletedAt": null
+            },
+            {
+                "name": "Kilogram",
+                "shortForm": "[kg]",
+                "materials": null,
+                "id": "c76baa0a-2078-4a0e-9ae5-9c1f28fdb922",
+                "isActive": true,
+                "isDeleted": false,
+                "createdAt": "2019-10-23T09:54:18.899245Z Etc/UTC",
+                "updatedAt": "2019-10-23T09:54:27.063285Z Etc/UTC",
+                "deletedAt": null
+            },
+            {
+                "name": "Metre",
+                "shortForm": "[m]",
+                "materials": null,
+                "id": "7488252e-6957-4663-a189-9a5b2d9b03b7",
+                "isActive": true,
+                "isDeleted": false,
+                "createdAt": "2019-10-23T09:54:38.083757Z Etc/UTC",
+                "updatedAt": null,
+                "deletedAt": null
+            }];
+        return (
+            <div className="ui container">
+                <DataTable headerText={['Name', 'ShortForm', 'Materials','id','Active','Deleted','Created At','Updated At','Deleted At']}
+                 data={data} />
+            </div>
+        )
+    }
 }
-export default List;
